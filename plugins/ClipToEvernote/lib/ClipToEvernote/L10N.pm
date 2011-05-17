0da0e636697440ccb2c3626574320dd3e6a7b9d7
Akira Sawada <asawada@sixapart.com>
2011-05-17 15:01:07 +0900

    L10Ned.

---
+++ plugins/ClipToEvernote/lib/ClipToEvernote/L10N.pm
@@ -0,0 +1,7 @@
+package ClipToEvernote::L10N;
+
+use strict;
+use warninigs;
+use base qw( MT::Plugin::L10N );
+
+1;
---
+++ plugins/ClipToEvernote/lib/ClipToEvernote/L10N/ja.pm
@@ -0,0 +1,24 @@
+package ClipToEvernote::L10N::ja;
+
+use strict;
+
+use base 'ClipToEvernote::L10N';
+use vars qw( %Lexicon );
+%Lexicon = (
+## plugins/ClipToEvernote/lib/ClipToEvernote.pm
+  'Evernote' => 'Evernote',
+  q{Evernote access is allowed only for entry's owner.} => q{ブログ記事の所有者以外はEvernoteへのアクセスは出来ません。},
+  'Failed to sync entry to Evernote.' => 'Evernoteとの同期に失敗しました。',
+
+## plugins/ClipToEvernote/tmpl/evernote_widget.tmpl
+  'Do not clip' => 'クリップしない',
+  'Sign Out' => 'サインアウト',
+  'Sign in to Evernote' => 'Evernoteにサインイン',
+  'View in Evernote' => 'Evernoteでブログ記事を見る',
+
+## plugins/ClipToEvernote/tmpl/system.tmpl
+  'Consumer Key' => 'コンシューマキー',
+  'Consumer Secret' => 'コンシューマシークレット',
+);
+
+1;
