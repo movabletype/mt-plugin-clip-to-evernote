bd02584d4045630f7c3576646352ba308d0beb11
kaminogoya <kaminogoya@gmail.com>
2011-05-18 15:48:31 +0900

    styling widget.

--- plugins/ClipToEvernote/lib/ClipToEvernote/L10N/ja.pm
+++ plugins/ClipToEvernote/lib/ClipToEvernote/L10N/ja.pm
@@ -12,9 +12,10 @@ use vars qw( %Lexicon );
 
 ## plugins/ClipToEvernote/tmpl/evernote_widget.tmpl
   'Do not clip' => 'クリップしない',
+  'Delete clip note' => 'クリップしたノートを削除',
   'Sign Out' => 'サインアウト',
-  'Sign in to Evernote' => 'Evernoteにサインイン',
-  'View in Evernote' => 'Evernoteでブログ記事を見る',
+  'Sign in' => 'サインイン',
+  'View in Evernote' => 'クリップしたノートを確認',
 
 ## plugins/ClipToEvernote/tmpl/system.tmpl
   'Consumer Key' => 'Consumer Key',
